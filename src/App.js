61f567689cb2e1dc32356b2d73f9289898537dd3
deggen <d.kellenschwiler@nchain.com>
2022-05-30 14:07:41 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+import React from "react";
+import { Game } from "./Game";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Calendar Puzzle</h1>
+      <Game />
+    </div>
+  );
+}
